6c33c6232be9bbf198f2eb316cea6da7d0064bef
vasuONgh <vasumailsy2k15@gmail.com>
2026-05-21 18:02:30 +0530

    first commit

---
+++ src/main/java/com/vasoo/springmongo/DemoApp.java
@@ -0,0 +1,12 @@
+package com.vasoo.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApp.class, args);
+    }
+}
---
+++ src/main/java/com/vasoo/springmongo/UserRepository.java
@@ -0,0 +1,7 @@
+package com.vasoo.springmongo;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+
+public interface UserRepository extends PagingAndSortingRepository<User, String> {
+}
